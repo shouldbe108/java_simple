cac13625608730e6a3d46664292625eccbc90cd1
shouldbe108 <47333841+shouldbe108@users.noreply.github.com>
2023-09-14 20:14:28 +0200

    Create EvenNumber.java
    check if number given is even or not. it is a simple program to understand TDD. it does not take user inputs. the test java file has predefined inputs in the test functions.

---
+++ EvenNumber.java
@@ -0,0 +1,19 @@
+package tddcourse;
+
+public class EvenNumber {
+
+	public static boolean checkNumIsEven(int number) {
+		if(number <=0) {
+			return false;
+		
+		}
+		if (number%2==0) {
+			return true;
+		} else {
+			return false;
+			
+    }
+
+	}
+
+}
